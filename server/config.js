e5da686ae23400dea0a99928bd4b7cc5a4f14c5a
Trung Hoang <trunghq3101@gmail.com>
2023-08-07 09:34:51 +0700

    WIP

--- server/config.js
+++ server/config.js
@@ -1,9 +1,16 @@
 module.exports = {
-	// FusionAuth info (copied from the FusionAuth admin panel)
-	clientID: 'e9fdb985-9173-4e01-9d73-ac2d60d1dc8e',
-	clientSecret: 'super-secret-secret-that-should-be-regenerated-for-production',
-	fusionAuthBaseUrl: 'http://localhost:9011',
+  // FusionAuth info (copied from the FusionAuth admin panel)
+  //   clientID: "a74e92dc-493d-41ce-8dcf-eb11d36e2ff6",
+  //   clientSecret: "I9R27A-6aEBPbqXvF7WfI4GMjygblrucQMJHp_xyHsU",
+  //   serverPort: 9000,
 
-	// port this server runs on
-	serverPort: 9000
+  //   clientID: "346a3a50-bb1f-45c8-b9cf-3201eb410138",
+  //   clientSecret: "RX9zrleh8r0hMfkLStGXwXirEFZzqAk0aaP-HoVe0UM",
+  //   serverPort: 9001,
+
+  clientID: "ee6a4019-b155-42b8-b425-54659adb2729",
+  clientSecret: "NBGZjWpa8EvpoXGOLeWxtXmAmp2dysdvip8TdvRpwLc",
+  serverPort: 9002,
+
+  fusionAuthBaseUrl: "http://localhost:9011",
 };
--- server/routes/callback.js
+++ server/routes/callback.js
@@ -1,58 +1,63 @@
-const express = require('express');
-const request = require('request');
-const config = require('../config.js');
-const cookie = require('../cookie.js');
-const redirectState = require('../redirectState.js');
+const express = require("express");
+const request = require("request");
+const config = require("../config.js");
+const cookie = require("../cookie.js");
+const redirectState = require("../redirectState.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   console.log("accepting request for token exchange");
+  console.log(req.query);
   const code = req.query.code;
   const codeVerifier = req.cookies.codeVerifier;
-  const redirect_uri = `${req.protocol}://${req.get('host')}/app/callback`;
+  const redirect_uri = `${req.protocol}://${req.get("host")}/app/callback`;
 
   request(
     // POST request to /oauth2/token endpoint
     {
-      method: 'POST',
+      method: "POST",
       uri: `${config.fusionAuthBaseUrl}/oauth2/token`,
       form: {
-        'client_id': config.clientID,
-        'client_secret': config.clientSecret,
-        'code': code,
-        'code_verifier': codeVerifier,
-        'grant_type': 'authorization_code',
-        'redirect_uri': redirect_uri,
+        client_id: config.clientID,
+        client_secret: config.clientSecret,
+        code: code,
+        code_verifier: codeVerifier,
+        grant_type: "authorization_code",
+        redirect_uri: redirect_uri,
       },
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     },
 
     (error, response, body) => {
-        const { access_token, id_token, refresh_token, expires_in } = JSON.parse(body);
-        if (access_token && refresh_token) {
-            console.log("saving tokens as cookies");
-            // save tokens as cookies
-            cookie.setSecure(res, 'app.at', access_token);
-            cookie.setSecure(res, 'app.rt', refresh_token);
+      const { access_token, id_token, refresh_token, expires_in } =
+        JSON.parse(body);
+      if (access_token && refresh_token) {
+        console.log("saving tokens as cookies");
+        // save tokens as cookies
+        cookie.setSecure(res, "app.at", access_token);
+        cookie.setSecure(res, "app.rt", refresh_token);
 
-            const expires_in_ms = expires_in * 1000;
-            cookie.setReadable(res, 'app.at_exp', Date.now() + expires_in_ms, expires_in_ms);
-            cookie.setReadable(res, 'codeVerifier', '', 0);
-            cookie.setReadable(res, "app.idt", id_token);
+        const expires_in_ms = expires_in * 1000;
+        cookie.setReadable(
+          res,
+          "app.at_exp",
+          Date.now() + expires_in_ms,
+          expires_in_ms
+        );
+        cookie.setReadable(res, "codeVerifier", "", 0);
+        cookie.setReadable(res, "app.idt", id_token);
 
-            const redirectUrl = redirectState.generateRedirectUrlFromState(req);
+        const redirectUrl = redirectState.generateRedirectUrlFromState(req);
 
-            res.redirect(redirectUrl);
-
-        } else {
-           console.log("Either refresh token or access token is missing.");
-           console.log(body);
-           res.sendStatus(503);
-        }
-
+        res.redirect(redirectUrl);
+      } else {
+        console.log("Either refresh token or access token is missing.");
+        console.log(body);
+        res.sendStatus(503);
+      }
     }
   );
 });
